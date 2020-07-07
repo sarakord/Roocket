060855c3f4de45b42d0c4539d913306707a41c08
sara <sarait405@gmail.com>
2020-07-07 23:23:01 +0430

    chart with Vue.js

--- resources/views/Admin/master.blade.php
+++ resources/views/Admin/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>وبسایت آموزشی لاراول</title>
 
     <link rel="stylesheet" href="{{asset('admin/css/admin.css')}}">
@@ -12,9 +12,13 @@
 
 <body>
 
+<div id="app">
     @include('Admin.section.header')
-        @yield('content')
-    @include('Admin.section.footer')
+    @yield('content')
+</div>
+
+@include('Admin.section.footer')
+
 </body>
 @include('sweetalert::alert')
 
--- resources/views/Admin/section/footer.blade.php
+++ resources/views/Admin/section/footer.blade.php
@@ -4,6 +4,6 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="{{asset('admin/js/admin.js')}}"></script>
+<script src="/js/app.js"></script>
 <script src="{{asset('admin/js/bootstrap-select.js')}}"></script>
 @yield('script')
