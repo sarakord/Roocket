b91f1eb07d944beebf30edee7c2180a10e7517b2
sara <sarait405@gmail.com>
2020-07-08 00:04:37 +0430

    factory with thinker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
+use App\Payment;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -24,5 +25,15 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Payment::class, function (Faker $faker) {
+    return [
+        'user_id' => rand(1,2),
+        'resnumber' =>  rand(1000000000,5000000000),
+        'price' => rand(10000,50000),
+        'payment' => rand(0 ,1),
+        'created_at' => $faker->dateTimeBetween('-6 months' , 'now'),
     ];
 });
