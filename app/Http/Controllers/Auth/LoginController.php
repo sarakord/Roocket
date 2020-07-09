818eebfc5e465bb2155b58bcb9bd6a8e02f16eb1
sara <sarait405@gmail.com>
2020-07-09 17:04:53 +0430

    Activation account - send Email - Event and listener

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Events\UserActivation;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -26,7 +30,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -37,4 +41,72 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        /*if (auth()->validate($request->only('email', 'password'))) {
+            $user = User::whereEmail($request->input('email'))->first();
+            if ($user->active == 0) {
+                $checkActiveCode = $user->activationCodes()->where('expire', '>=', Carbon::now())->latest()->first();
+
+                if (count($checkActiveCode) == 1) {
+                    if ($checkActiveCode->expire > Carbon::now()) {
+                        $this->incrementLoginAttempts($request);
+                        return back()->withErrors(['code' => 'ایمیل فعال سازی قبلا به ایمیل شما ارسال شد بعد از 15 دقیقه دوباره برای ارسال ایمیل لاگین کنید']);
+                    }
+                } else
+                    event(new UserActivation($user));
+            }
+        }*/
+
+        if(auth()->validate($request->only('email' , 'password'))) {
+            $user = User::whereEmail($request->input('email'))->first();
+            if($user->active == 0 ) {
+                $checkActiveCode = $user->activationCodes()->where('expire' , '>=' , Carbon::now() )->latest()->first();
+                $count= $checkActiveCode != null ? '1' : '0' ;
+                if($count == 1) {
+                    if($checkActiveCode->expire > Carbon::now() ) {
+                        $this->incrementLoginAttempts($request);
+                        return back()->withErrors(['code' => 'ایمیل فعال سازی قبلا به ایمیل شما ارسال شد بعد از 15 دقیقه دوباره برای ارسال ایمیل لاگین کنید']);
+                    }
+                } else {
+                    event(new UserActivation($user));
+                }
+            }
+        }
+
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
 }
--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'mailtrap.io'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 2525),
+    'port' => env('MAIL_PORT', 587),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'info@roocket.com'),
-        'name' => env('MAIL_FROM_NAME', 'راکت'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('a2a0befd254a8e'),
+    'username' => env('MAIL_USERNAME'),
 
-    'password' => env('700a08a93e14d3'),
+    'password' => env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
